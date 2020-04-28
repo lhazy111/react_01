8cc4c83b0a3ce043ae5c3dd7fef9675ed855835e
lhazy111 <lhazy111@googlemail.com>
2020-04-28 22:27:05 +0100

    search ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -19,9 +20,13 @@ class App extends React.Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={filteredMonsters} />
 
 
       </div>
--- my-app/src/components/card/card.component.jsx
+++ my-app/src/components/card/card.component.jsx
@@ -3,6 +3,8 @@ import './card.styles.css'
 
 export const Card = (props) => (
     <div className='card-container'>
+        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
         <h1>{props.monster.name}</h1>
-    </div>
+        <p>{props.monster.email}</p>
+    </div >
 );
